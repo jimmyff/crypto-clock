6467e0577b01279eb6b4186273844735ad2e03f2
Jimmy Forrester-Fellowes <code@jimmyff.uk>
2021-07-20 09:34:36 +0100

    Added timezone support. Flipped sun & moon direction of travel

--- web/settings.dart
+++ web/settings.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/browser_client.dart' as http;
+import 'package:timezone/browser.dart' as tz;
 
 /// Handles loading & saving of settings and settings UI
 class CryptoClockSettings {
@@ -11,6 +12,13 @@ class CryptoClockSettings {
   //settings
   late List<Map<String, dynamic>> symbols;
   late List<Map<String, dynamic>> symbolsDraft;
+  String? timezone;
+  late tz.Location? timezoneLocation;
+  final Completer timezoneInit = Completer();
+
+  /// periodic refresh
+  late int refresh;
+  List<int> refreshOptions = [0, 2, 4, 6, 12, 24];
   late int interval;
   late int intervalDraft;
   List<int> intervalOptions = [5, 10, 20, 30, 45, 60, 90, 120, 240, 300, 600];
@@ -19,6 +27,7 @@ class CryptoClockSettings {
   List<int> intervalsPerSymbolDraftOptions = List.generate(20, (i) => i + 1);
 
   // UI
+  late Element refreshElement;
   late Element intervalElement;
   late Element intervalsPerSymbolElement;
 
@@ -47,20 +56,24 @@ class CryptoClockSettings {
       'quoteAsset': 'BTC',
     },
   ];
-  CryptoClockSettings(this.client) {
+  CryptoClockSettings(this.client, this.timezone) {
     symbols = [];
     _initSettingsFromStorage();
+    _initTimezone();
   }
 
   void _saveSettings() {
+    window.localStorage['refresh'] =
+        refreshElement.text! == 'Off' ? '0' : refreshElement.text!;
     window.localStorage['symbols'] = json.encode(symbolsDraft);
     window.localStorage['interval'] = intervalElement.text!;
     window.localStorage['intervalsPerSymbol'] = intervalsPerSymbolElement.text!;
-    window.location.reload();
+    refreshWindow();
   }
 
   void _initSettingsFromStorage() {
     // Load settings/defaults
+    window.localStorage['refresh'] ??= '0';
     window.localStorage['symbols'] ??= json.encode(_defaultSymbols);
     window.localStorage['interval'] ??= _defaultInterval.toString();
     window.localStorage['intervalsPerSymbol'] ??=
@@ -70,10 +83,51 @@ class CryptoClockSettings {
     symbolsDraft =
         (json.decode(window.localStorage['symbols']!) as List<dynamic>)
             .cast<Map<String, dynamic>>();
+    refresh = int.parse(window.localStorage['refresh']!);
     interval = int.parse(window.localStorage['interval']!);
     intervalsPerSymbol = int.parse(window.localStorage['intervalsPerSymbol']!);
 
     _initDom();
+  }
+
+  void _initTimezone() async {
+    if (timezone != null) {
+      try {
+        await tz.initializeTimeZone();
+        timezoneLocation = tz.getLocation(timezone!);
+      } catch (_) {
+        window.alert('Failed to load timezone "$timezone"');
+        timezone = null;
+        timezoneLocation = null;
+      }
+    } else {
+      timezoneLocation = null;
+    }
+    timezoneInit.complete();
+  }
+
+  void refreshWindow() {
+    window.localStorage['refreshedWindow'] = DateTime.now().toString();
+    window.location.reload();
+  }
+
+  /// Checks if we progmatically refreshed within the last minute
+  bool recentlyRefreshed() {
+    if (window.localStorage['refreshedWindow'] == null) return false;
+
+    final refreshed = DateTime.parse(window.localStorage['refreshedWindow']!);
+    return refreshed.add(Duration(minutes: 1)).isAfter(DateTime.now());
+  }
+
+  /// Returns bool if intro should be shown.
+  bool showIntro() {
+    return !recentlyRefreshed();
+  }
+
+  Future<DateTime> getTime() async {
+    if (!timezoneInit.isCompleted) await timezoneInit.future;
+    if (timezone == null) return DateTime.now().toLocal();
+    return tz.TZDateTime.now(timezoneLocation!);
   }
 
   Future _loadAllSymbols() async {
@@ -124,7 +178,7 @@ class CryptoClockSettings {
       window.localStorage['interval'] = _defaultInterval.toString();
       window.localStorage['intervalsPerSymbol'] =
           _defaultIntervalsPerSymbol.toString();
-      window.location.reload();
+      refreshWindow();
     });
     querySelector('#close')!.onClick.listen((event) {
       querySelector('#settings')!.classes.remove('visible');
@@ -134,7 +188,7 @@ class CryptoClockSettings {
     });
 
     querySelector('#refresh')!.onClick.listen((event) {
-      window.location.reload();
+      refreshWindow();
     });
 
     querySelector('#load_symbols')!.onClick.listen((event) {
@@ -144,6 +198,8 @@ class CryptoClockSettings {
 
     _refreshSymbolsDraft();
 
+    refreshElement = querySelector('#refresh_value')!
+      ..innerText = refresh == 0 ? 'Off' : refresh.toString();
     intervalElement = querySelector('#interval')!
       ..innerText = interval.toString();
     intervalsPerSymbolElement = querySelector('#intervals_per_element')!
@@ -185,6 +241,28 @@ class CryptoClockSettings {
       intervalElement.innerText =
           _changeValue(int.parse(intervalElement.innerText), intervalOptions, 1)
               .toString();
+    });
+
+    // picker: refresh
+    querySelector('#refresh_back')!.onClick.listen((event) {
+      final val = _changeValue(
+              int.parse(refreshElement.innerText == 'Off'
+                  ? '0'
+                  : refreshElement.innerText),
+              refreshOptions,
+              -1)
+          .toString();
+      refreshElement.innerText = val == '0' ? 'Off' : val;
+    });
+    querySelector('#refresh_forward')!.onClick.listen((event) {
+      final val = _changeValue(
+              int.parse(refreshElement.innerText == 'Off'
+                  ? '0'
+                  : refreshElement.innerText),
+              refreshOptions,
+              1)
+          .toString();
+      refreshElement.innerText = val == '0' ? 'Off' : val;
     });
   }
 }
